11dcdd99b002faf9ceeed3a781f0c705c8623d9a
jfisherge <jfisherge@users.noreply.github.com>
2017-06-20 12:38:03 -0400

    Update spog-controller.js

--- spog-controller.js
+++ spog-controller.js
@@ -40,15 +40,15 @@ class controller {
 		$http.get('contextbrowser/api/accessibleResources')
 	        .then(function(response){
 	            if(response.data.length>0){
-	                for(let ii=0;ii<response.data.length;ii++){
+	                for(var i=0;i<response.data.length;i++){
 	                    //add identifiers to children
-	                    response.data[ii]['identifier'] = '00-' + ii;
-	                    response.data[ii]['hasChildren'] = true;
-	                    response.data[ii]['isOpenable'] = true;
+	                    response.data[i]['identifier'] = '00-' + i;
+	                    response.data[i]['hasChildren'] = true;
+	                    response.data[i]['isOpenable'] = true;
 	                }
 	                response.data.sort(sortResponseDataByNameProperty);
 	                browser.browserContext=initialContext(response.data);
-	                let data = JSON.parse(window.sessionStorage.getItem('selectedIds'));
+	                var data = JSON.parse(window.sessionStorage.getItem('selectedIds'));
 					if(data){
 		                persistingContextBrowser('enterprises');
 		            }
@@ -57,7 +57,7 @@ class controller {
 		            // until APM fixes the bug in accessibleResources
 		            $http.get('contextbrowser/api/allInstances?parent=null&components=BASIC&sortBy=NAME')
 			            .then(function(response2) {
-				            for(let ii=0;ii<response2.data.length;ii++){
+				            for(var ii=0;ii<response2.data.length;ii++){
 					            //add identifiers to children
 					            response2.data[ii]['identifier'] = '00-' + ii;
 					            response2.data[ii]['hasChildren'] = true;
@@ -65,18 +65,18 @@ class controller {
 				            }
 				            response2.data.sort(sortResponseDataByNameProperty);
 				            browser.browserContext=initialContext(response2.data);
-				            let data = JSON.parse(window.sessionStorage.getItem('selectedIds'));
-				            if(data){
+				            var data2 = JSON.parse(window.sessionStorage.getItem('selectedIds'));
+				            if(data2){
 					            persistingContextBrowser('enterprises');
 				            }
 			            }, function(data) {
-				            // Handle error here
-				            closeSpinner();
+			            // Handle error here
+			            closeSpinner();
 			            });
 	            }
 	        }, function(data) {
-			// Handle error here
-			closeSpinner();
+				// Handle error here
+				closeSpinner();
 		});
 
 	    browser.handlers = {
